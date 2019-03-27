eb5ec68202ea39e5c0837158d28fd3c2cc50c5d4
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-03-26 21:30:32 -0300

    Aula calcula IMC

--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,43 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+//IMC 
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = paciente.querySelector(".info-peso").textContent;
+for( var i = 0; i < pacientes.length; i++){
 
-var altura = paciente.querySelector(".info-altura").textContent;
+    var paciente = pacientes[i];
 
-var imc = paciente.querySelector(".info-imc");
+    var peso = paciente.querySelector(".info-peso").textContent;
 
-var result = peso / (altura * altura);
+    var altura = paciente.querySelector(".info-altura").textContent;
 
-imc.textContent = result; 
+    var imc = paciente.querySelector(".info-imc");
 
+    var result = peso / (altura * altura);
+    
+    imc.textContent = result; 
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 0 || peso > 100){
+        pesoEhValido = false;
+        imc.textContent = "Peso Inválido";
+    }
+
+    if(altura <= 0 || altura >= 3.00){
+        alturaEhValida = false;
+        imc.textContent = "Altura Inválido";
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var result = peso / (altura * altura);
+        imc.textContent = result;
+    }else{
+        imc.textContent = "Peso e/ou altura inválidos";
+    }
+}
+
+
+   
